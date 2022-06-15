b541c16adf93f0f3cc92b6ef50402ffba109ab4f
tada195908 <48470775+tada195908@users.noreply.github.com>
2022-06-15 09:45:49 +0900

    Add files via upload

--- src/ctrw1/Walker.java
+++ src/ctrw1/Walker.java
@@ -9,7 +9,9 @@ import randomNumberGenerators.AbstractRandom;
  * @author tadaki
  */
 public class Walker extends AbstractWalker {
-protected Set<AbstractWalker> stoppedWalkers;
+
+    protected Set<AbstractWalker> stoppedWalkers;
+
     public Walker(AbstractRandom waitingTimeRandom, AbstractRandom motionRandom,
             double p, int label) {
         super(waitingTimeRandom, motionRandom, p, label);
